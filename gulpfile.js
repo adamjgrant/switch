dcb15d2ecae7439f7fcf3e1dd8cf3f6d0a9e9028
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-10-21 14:17:28 -0400

    Done.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass');
+  sass = require('gulp-ruby-sass'),
+  connect = require('gulp-connect');
 
-gulp.task('default', function() {
+gulp.task('default', ['connect'], function() {
   gulp.src(['example.sass'])
     .pipe(sass())
     .pipe(gulp.dest('./'))
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    root: './',
+    livereload: true
+  })
+})
